31a21c8fd13114b6d5113114903ee7e49b8c6bcf
keter1968 <ia_mendez@yahoo.com>
2017-08-28 21:20:13 -0500

    inicial design of components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { CalculateComponent } from './calculate/calculate.component';
+import { SettingsComponent } from './settings/settings.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    CalculateComponent,
+    SettingsComponent
   ],
   imports: [
     BrowserModule
